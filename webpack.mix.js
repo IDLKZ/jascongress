9ec5e754c0b6b413f5684e43682c090f82190f7b
IDLKZ <idlkz2019@gmail.com>
2021-06-02 17:53:43 +0600

    first commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,50 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+// mix.js('resources/js/app.js', 'public/js')
+//     .sass('resources/sass/app.scss', 'public/css');
+
+// mix.styles([
+//     'resources/assets/css/bootstrap.min.css',
+//     'resources/assets/css/magnific-popup.css',
+//     'resources/assets/css/jquery.selectBox.css',
+//     'resources/assets/css/dropzone.css',
+//     'resources/assets/css/rangeslider.css',
+//     'resources/assets/css/animate.min.css',
+//     'resources/assets/css/leaflet.css',
+//     'resources/assets/css/slick.css',
+//     'resources/assets/css/slick-theme.css',
+//     'resources/assets/css/slick-theme.css',
+//     'resources/assets/css/map.css',
+//     'resources/assets/css/jquery.mCustomScrollbar.css',
+//     'resources/assets/fonts/font-awesome/css/font-awesome.min.css',
+//     'resources/assets/fonts/flaticon/font/flaticon.css',
+//     'resources/assets/css/style.css',
+//     'resources/assets/css/skins/default.css'
+// ], 'public/css/app.css')
+//
+// mix.scripts([
+//     'resources/assets/js/jquery-2.2.0.min.js',
+//     'resources/assets/js/popper.min.js',
+//     'resources/assets/js/bootstrap.min.js',
+//     'resources/assets/js/jquery.selectBox.js',
+//     'resources/assets/js/rangeslider.js',
+//     'resources/assets/js/jquery.magnific-popup.min.js',
+//     'resources/assets/js/jquery.filterizr.js',
+//     'resources/assets/js/wow.min.js',
+//     'resources/assets/js/backstretch.js',
+//     'resources/assets/js/jquery.countdown.js',
+//     'resources/assets/js/jquery.scrollUp.js',
+//     'resources/assets/js/particles.min.js',
+//     'resources/assets/js/typed.min.js',
+//     'resources/assets/js/dropzone.js',
+//     'resources/assets/js/jquery.mb.YTPlayer.js',
+//     'resources/assets/js/leaflet.js',
+//     'resources/assets/js/leaflet-providers.js',
+//     'resources/assets/js/leaflet.markercluster.js',
+//     'resources/assets/js/slick.min.js',
+//     'resources/assets/js/maps.js',
+//     'resources/assets/js/jquery.mCustomScrollbar.concat.min.js',
+//     'resources/assets/js/ie-emulation-modes-warning.js',
+//     'resources/assets/js/app.js'
+// ], 'public/js/app.js')
